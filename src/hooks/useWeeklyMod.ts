f4f386a07ff5475e9ec355191fe6d32a6fa769b7
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-11-11 10:01:08 -0600

    Dato extraidos desde una api

--- src/hooks/useWeeklyMod.ts
+++ src/hooks/useWeeklyMod.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import type { WeeklyModData } from '../types/WeeklyModData';
+import { weeklyService } from '../api/services/WeeklyModService';
 
 export const useWeeklyModData = () => {
     const [data, setData] = useState<WeeklyModData[]>([]);
@@ -11,50 +12,9 @@ export const useWeeklyModData = () => {
             setLoading(true);
             setError(null);
 
-            // Simulamos una llamada a API - reemplaza con tu endpoint real
-            // const response = await fetch('/api/weekly-mod');
-            // const result: WeeklyModData[] = await response.json();            
+            const response = await weeklyService.getAll()
 
-            const sampleData: WeeklyModData[] = [
-                {
-                    "id": 5,
-                    "weekNumber": 46,
-                    "materialType": "CU",
-                    "productivityTarget": 5.2,
-                    "productionVolume": 40000,
-                    "hoursNeed": 7692,
-                    "mod": 311,
-                    "hoursPersonAvailable": 14461,
-                    "excessPersonHours": 6769,
-                    "excessHoursPerPerson": 21.77
-                },
-                {
-                    "id": 6,
-                    "weekNumber": 46,
-                    "materialType": "AL",
-                    "productivityTarget": 5.2,
-                    "productionVolume": 7000,
-                    "hoursNeed": 1346,
-                    "mod": 70,
-                    "hoursPersonAvailable": 3255,
-                    "excessPersonHours": 1909,
-                    "excessHoursPerPerson": 27.27
-                },
-                {
-                    "id": 7,
-                    "weekNumber": 47,
-                    "materialType": "CU",
-                    "productivityTarget": 5.5,
-                    "productionVolume": 45000,
-                    "hoursNeed": 8182,
-                    "mod": 330,
-                    "hoursPersonAvailable": 15000,
-                    "excessPersonHours": 6818,
-                    "excessHoursPerPerson": 20.66
-                }
-            ];
-
-            setData(sampleData);
+            setData(response);
         } catch (err) {
             setError(err instanceof Error ? err.message : 'Error al cargar los datos');
         } finally {
